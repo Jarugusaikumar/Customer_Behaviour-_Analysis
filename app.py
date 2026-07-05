b26911df2f6b1bfc588d4ad0bf281af2febf1a8b
Jarugusaikumar <167310170+Jarugusaikumar@users.noreply.github.com>
2026-07-05 19:52:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,10 @@ st.dataframe(data)
 
 st.write("### Summary Statistics")
 st.write(data.describe())
+
+
+st.title("📊 Customer Behaviour Dashboard")
+
+powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=37926c74-62d8-46d5-a100-a573e40cdad9&autoAuth=true&ctid=68a6b772-3760-49f5-b2d2-6a292154f999"
+st.components.v1.iframe(powerbi_url, height=800, scrolling=True)
+
